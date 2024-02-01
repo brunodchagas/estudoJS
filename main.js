7b79667a36c707c8213f10d1af3af62977966421
brunodchagas <brunodchagas@yahoo.com.br>
2024-01-31 22:04:01 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var nome = 20
+console.log(typeof(nome));
+
+if(typeof(nome) == "string"){
+    console.log(nome.toLocaleUpperCase());
+}else{
+    console.log("Você precisa adicionar uma string")
+}
---
+++ oper.js
@@ -0,0 +1,34 @@
+var n = 3;
+//n = n * 10;
+n*= 10;
+console.log(n);
+
+var x = 10;
+x++
+
+b = 20;
+c = 60;
+a=10;
+
+console.log(x);
+
+console.log(!(n <= x || b !=c));
+
+//varidar se o a é maior  que b e se b é divisivel por 2
+
+console.log(a>b && b % 2 == 0);
+
+
+//verificar se b é maior ou igual a a ou b é dividido por 2 o resultado é 2
+
+console.log(a<=b || b/2 ==2);
+
+
+//verificar se a é maior q b e b é maior ou igual a 5
+
+console.log(a>b && 5<=b);
+
+
+// verificar se b é par ou impar usando ternario
+
+resultado = b % 2 == 0 ? "PAR" : "IMPAR"
